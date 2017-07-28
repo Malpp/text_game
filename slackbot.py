fe4597415276f4094475448ec982f482b4e2e83f
malpp <malpp@users.noreply.github.com>
2017-07-28 16:32:53 -0400

    only send back non-bot messages

--- slackbot.py
+++ slackbot.py
@@ -38,11 +38,11 @@ def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output:
+            if output and 'text' in output and output['user'] != BOT_ID:
                 slack_client.api_call(
                     "chat.postMessage", channel=output['channel'],
                     text=json.dumps(output), as_user=True)
-                    
+
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
