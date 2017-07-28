26165e9a6dd9081e5f17ba67379f6d80ba038d23
malpp <malpp@users.noreply.github.com>
2017-07-28 13:16:14 -0400

    added port

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 
 import json
 import requests
+import os
 from flask import Flask, request, make_response
 
 app = Flask(__name__)
@@ -10,7 +11,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+	port = int(os.getenv('PORT', 5000))
+	app.run(debug=True, port=port, host='0.0.0.0')
